1bcd3cc5232aea4db4fa7fe6c9580ece5b8d7885
ravikumar7007 <ramukivar7007@gmail.com>
2024-04-13 22:32:27 +0530

    added another output

--- .github/actions/deploy-s3-js/index.js
+++ .github/actions/deploy-s3-js/index.js
@@ -11,6 +11,9 @@ function run() {
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 
   core.notice("Hello from my custom JS action");
+
+  const url = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput('website-url',url);
 }
 
 run();
