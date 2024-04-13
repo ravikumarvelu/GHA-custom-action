50c27634642d36f9448f912de1d84eb007ecf4ff
RAVI KUMAR V <147708804+ravikumarvelu@users.noreply.github.com>
2024-04-13 14:20:41 +0000

    mjs

--- .github/actions/deploy-s3-js/index.js
+++ .github/actions/deploy-s3-js/index.js
@@ -1,8 +1,8 @@
-import core from '@actions/core'
-import github from '@actions/github'
-import exec from '@actions/exec'
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
 
-function run(){
+function run() {
     core.notice("Hello from my custom JS action");
 }
 
